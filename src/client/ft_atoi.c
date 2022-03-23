f5b947a52c819b25b34ced0b8374a2fdeb2e0ee1
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-03-23 12:52:19 +0100

    atoi got a separate file

---
+++ src/client/ft_atoi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_atoi.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/03/23 12:03:38 by pmolnar       #+#    #+#                 */
+/*   Updated: 2022/03/23 12:03:39 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+static int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+static int	ft_isspace(int c)
+{
+	return (c == ' '
+		|| c == '\t'
+		|| c == '\n'
+		|| c == '\v'
+		|| c == '\f'
+		|| c == '\r');
+}
+
+int	ft_atoi(const char *str)
+{
+	int			num;
+	int			sign;
+	const int	coeff[2] = {+1, -1};
+
+	num = 0;
+	sign = 1;
+	while (*str && ft_isspace(*str))
+		str++;
+	if (*str && (*str == '+' || *str == '-'))
+		sign = coeff[(*str++ == '-')];
+	while (*str && ft_isdigit(*str))
+		num = 10 * num + (*str++ - '0');
+	return (sign * num);
+}
